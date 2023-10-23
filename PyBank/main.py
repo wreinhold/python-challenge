48b04fbaa5c820215b30ba8041e4ec060d895e78
wreinhold <will@MacBook-Air.local>
2023-10-23 19:59:22 -0400

    Added comments to my code for readability

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,40 +1,49 @@
-from audioop import avg
-from email import header
 import os
 import csv
 
+# Set up path to read csv 
 bank_csv = os.path.join("Resources","budget_data.csv")
 
+# Opened & Read file into reader
 with open(bank_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
+    # Set up variables for future use
     total_months = 0
     total_profit= 0
     avg_change = 0
     gain_max = 0
     loss_max = 0
 
+    # Took in Header data
     header = next(csv_reader)
 
+    # Created a loop to count the total months (rows) in the data set & calculated the total profit in the set
     for row in csv_reader:
         total_months += 1
         total_profit = total_profit + int(row[1])
+        
+        # Searched for the max gain by comparing the entry to the current highest
         if int(row[1]) > int(gain_max):
             gain_max = row[1]
             gain_date = row[0]
+        # performed the inverse of above for the max loss
         if int(row[1]) < int(loss_max):
             loss_max = row[1]
             loss_date = row[0]
 
+    # Printed out the results of the analysis to the user in the terminal
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {total_months}")
     print(f"Total: ${total_profit}")
+    # Calculated the average change for each month
     avg_change = total_profit / total_months
     print(f"Average Change: ${avg_change}")
     print(f"Greatest Increase in Profits: {gain_date} (${gain_max})")
     print(f"Greatest Decrease in Profits: {loss_date} (${loss_max})")
 
+# Output the same results as above to an analysis text file for the user
 bank_output = os.path.join("Analysis","analysis.txt")
 with open(bank_output,'w') as f:
     f.write("Financial Analysis\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,24 @@
 import os
 import csv
 
+# Set up path to read csv 
 poll_csv = os.path.join("Resources","election_data.csv")
 
+# Opened & Read file into reader
 with open(poll_csv) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
+    # Set up variables for future use
     total_votes = 0
     candidates= {}
     highest_votes = 0
     winner = ""
 
+    # Took in Header data
     header = next(csv_reader)
 
+    # Counted total number of votes using for loop and filled the candidates dictionary based on if the candidate was already in
+    # the dictionary. Added plus one for every occurance of the candidates name
     for row in csv_reader:
         total_votes += 1
         if row[2] not in candidates:
@@ -20,9 +26,11 @@ with open(poll_csv) as csvfile:
         else:
             candidates[row[2]] += 1
           
+    # Printed out the results of the election to the terminal
     print("Election Analysis")
     print("----------------------------")
     print(f"Total Votes: {total_votes}")
+    # Created a loop to list the candidate, calculate the vote percentage, and determine the winner
     for candidate in candidates:
         print(f"{candidate}: {round(candidates[candidate]/int(total_votes) * 100, 3)}% ({candidates[candidate]})")
         if int(candidates[candidate]) > int(highest_votes):
@@ -32,6 +40,7 @@ with open(poll_csv) as csvfile:
     print(f"Winner: {winner}")
     print("----------------------------")
 
+# Output the same results as above to an analysis text file
 poll_output = os.path.join("Analysis","analysis.txt")
 with open(poll_output,'w') as f:
     f.write("Election Analysis\n")
